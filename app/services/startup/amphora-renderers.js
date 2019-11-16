9161381afbdaf4ea800693789224a9309529948d
Philip Olson <phil.olson@swarmsolutions.com>
2019-11-16 11:51:45 -0600

    poc

--- app/services/startup/amphora-renderers.js
+++ app/services/startup/amphora-renderers.js
@@ -2,8 +2,19 @@
 
 const pkg = require('../../package.json'),
   amphoraHtml = require('amphora-html'),
+  setup = require('amphora-html/lib/setup'),
   helpers = require('../universal/helpers'),
-  resolveMediaService = require('../server/resolve-media');
+  resolveMediaService = require('../server/resolve-media'),
+  fs = require('fs'),
+  path = require('path');
+
+// init is necessary because their code sets module.exports.hbs to undefined
+//   after they init it themselves.  Granted, I don't know why they do that.
+setup.init();
+setup.hbs.registerPartial(
+  'testing',
+  fs.readFileSync(path.resolve(__dirname, './testing.hbs'), 'utf8')
+);
 
 amphoraHtml.configureRender({
   editAssetTags: true,
